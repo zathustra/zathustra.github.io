1f046f8bef89aa50fb7e886f1372398dc24a758b
Uwe Mueller <mueller.uwe@gmail.com>
2019-01-24 18:48:34 -0500

    Finish the JavaScript gallery

--- _site/js/gallery.js
+++ _site/js/gallery.js
@@ -1,4 +1,27 @@
 
 function activateGallery() {
-	alert("Hello from the gallery file!")
+	let thumbnails 	= document.querySelectorAll("#gallery-thumbs > div > img");
+	let mainImage 	= document.querySelector("#gallery-photo img")
+
+	thumbnails.forEach(function(thumbnail) {
+		thumbnail.addEventListener("click", function() {
+			// set clicked image as main image
+			let newImageSrc = thumbnail.dataset.largeVersion;
+			let altImage 	= thumbnail.dataset.title;
+			mainImage.setAttribute("src", newImageSrc);
+			mainImage.setAttribute("alt", altImage);
+
+			// Change wich image is current
+			const currentClass = "current"
+			document.querySelector(".current").classList.remove(currentClass);
+			thumbnail.parentNode.classList.add(currentClass);
+
+			//Update image info
+			let galleryInfo = document.querySelector('#gallery-info');
+			let title 		= galleryInfo.querySelector(".title");
+			let description = galleryInfo.querySelector(".description");
+			title.innerHTML 		= thumbnail.dataset.title;
+			description.innerHTML 	= thumbnail.dataset.description;
+		});
+	});
 }
